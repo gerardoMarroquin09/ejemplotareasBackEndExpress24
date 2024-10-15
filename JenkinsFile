4906c3570d5773c825b1af5bb7179703bbc9c01d
gerardoMarroquin09 <ma19074@ues.edu.sv>
2024-10-14 19:07:05 -0600

    Para hacer Pruebas de Jenkins
    Si

---
+++ JenkinsFile
@@ -0,0 +1,11 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('preparacion'){
+            steps {
+                echo 'hola mundo'
+            }
+        }
+    }
+}
---
+++ test/1holamundo.js
@@ -0,0 +1,13 @@
+import { assert } from 'chai';
+// const server = require ('../server')
+// import chai from 'chai';
+// import chaiHttp from 'chai-http';
+// Haremos una prueba sencilla. Nota la estructura de la prueba en mocha.
+// Tenemos un describe que es una descripción de un conjunto de pruebas,
+// y dentro de él tenemos un it que es una prueba en sí.
+
+describe('01 prueba hola mundo', () => {
+    it('estoy haciendo la primera prueba', () => {
+        assert.equal(true, true);
+    });
+});
